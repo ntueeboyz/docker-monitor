8d265fc93b99553301ba112d0e50d9e83f37541b
mingkuo <32310076+NTUEEboy@users.noreply.github.com>
2023-06-28 00:43:26 +0800

    feat: use nest config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContainersModule } from './docker/containers/containers.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ContainersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev', '.env'],
+      isGlobal: true,
+    }),
+    ContainersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
